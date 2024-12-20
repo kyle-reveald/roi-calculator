2fab34331a376c0811501ceb3adf11266fb50149
kyle-reveald <119618280+kyle-reveald@users.noreply.github.com>
2024-12-19 21:46:03 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ROICalculator from './calculator';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ROICalculator />, document.getElementById('root'));
+```
